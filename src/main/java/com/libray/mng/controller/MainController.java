3a9a72cd15281dd38c8cebfb5d5d201b38a6b368
idrak07 <idrak.10225@gmail.com>
2021-04-06 16:23:09 +0600

    CRUD done for books

--- src/main/java/com/libray/mng/controller/MainController.java
+++ src/main/java/com/libray/mng/controller/MainController.java
@@ -3,9 +3,7 @@ package com.libray.mng.controller;
 import com.libray.mng.model.Book;
 import com.libray.mng.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +14,7 @@ public class MainController {
     @Autowired
     private BookService bookService;
 
-
+    /*get All Books*/
     @RequestMapping("/books/{id}")
     public Book getBooks(@PathVariable String id){
         return bookService.getBook(id);
@@ -26,4 +24,20 @@ public class MainController {
     public List<Book> getAllBooks(){
         return bookService.getBooks();
     }
+
+    @RequestMapping(method = RequestMethod.POST , value = "/books")
+    public void addBook(@RequestBody Book book){
+        bookService.addBook(book);
+    }
+    @RequestMapping(method = RequestMethod.PUT , value = "/books/{id}")
+    public void updateBook(@RequestBody Book book, @PathVariable String id){
+        bookService.updateBook(id, book);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE , value = "/books/{id}")
+    public void deleteBook(@PathVariable String id){
+        bookService.deleteBook(id);
+    }
+
+
 }
--- src/main/java/com/libray/mng/services/BookService.java
+++ src/main/java/com/libray/mng/services/BookService.java
@@ -3,22 +3,39 @@ package com.libray.mng.services;
 import com.libray.mng.model.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BookService {
-    List<Book> books= Arrays.asList(
+    List<Book> books= new ArrayList<>(Arrays.asList(
             new Book("book-01", "C++", 10, 100),
             new Book("book-02", "Java", 5, 520),
             new Book("book-03", "C#", 7, 340),
             new Book("book-04", "PHP", 8, 150)
-    );
+    ));
     public List<Book> getBooks(){
         return books;
     }
 
     public Book getBook(String id){
         return books.stream().filter(t->t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+    public void updateBook(String id, Book book){
+        for (int i=0 ; i<books.size(); i++){
+            Book b = books.get(i);
+            if(b.getId().equals(id)){
+                books.set(i, book);
+                return;
+            }
+        }
+    }
+    public void deleteBook(String id){
+        books.removeIf(t-> t.getId().equals(id));
     }
 }
